506106e1b3555ff4f3ce82fe2b57a16aa4f11342
yuejiewang <kathywangyuejie@gmail.com>
2025-07-30 16:12:57 +0800

    dsl init

---
+++ flagcx/tools/utils.py
@@ -0,0 +1,44 @@
+from typing import *
+from enum import *
+from itertools import count
+
+
+class Collective(Enum):
+    P2P = 0
+    Custom = 1
+    Broadcast = 2
+    Gather = 3
+    Scatter = 4
+    Reduce = 5
+    AllReduce = 6
+    AllGather = 7
+    ReduceScatter = 8
+    AlltoAll = 9
+    AlltoAllv = 10
+    Nop = 11
+
+
+class RedOp(Enum):
+    sum = 0
+    prod = 1
+    max = 2
+    min = 3
+    avg = 4
+    nop = 5
+
+
+class Instr(Enum):
+    P2P = 0
+    Custom = 1
+    Broadcast = 2
+    Gather = 3
+    Scatter = 4
+    Reduce = 5
+    AllReduce = 6
+    AllGather = 7
+    ReduceScatter = 8
+    AlltoAll = 9
+    AlltoAllv = 10
+    Nop = 11
+    LocCpy = 12
+    LocRed = 13
