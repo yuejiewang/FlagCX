58e8e6132e7598dfe60648f98d26ba2fd44c523e
MC952-arch <mc952-arch@qq.com>
2025-09-04 16:53:00 +0800

    Add a hook to support batch_isend_irecv in PyTorch 2.7 and newer (#235)

--- plugin/torch/flagcx/__init__.py
+++ plugin/torch/flagcx/__init__.py
@@ -38,18 +38,30 @@ def replace_device(module, fn_name):
         setattr(module, fn_name, replace_device_args(fn))
 
 def _batch_isend_irecv(p2p_op_list):
+    if not isinstance(p2p_op_list, list) or not all(
+        isinstance(p2p_op, dist.P2POp) for p2p_op in p2p_op_list
+    ):
+        raise ValueError(
+            "Invalid ``p2p_op_list``. Each op is expected to "
+            "to be of type ``torch.distributed.P2POp``."
+        )
+
     group = p2p_op_list[0].group
     device = p2p_op_list[0].tensor.device
-    is_multi_pg = True
-    if device.type == "cuda" or device.type == "npu":
-        with _coalescing_manager(group, device, async_ops=True) as cm:
-            for p2p_op in p2p_op_list:
-                p2p_op.op(p2p_op.tensor, p2p_op.peer, p2p_op.group, p2p_op.tag)
-        return cm.works
+    if not all(group == p2p_op.group for p2p_op in p2p_op_list):
+        raise ValueError("All ops need to use the same group.")
+
+    # we adopt the implementation of PyTorch 2.5
+    with _coalescing_manager(group, device, async_ops=True) as cm:
+        for p2p_op in p2p_op_list:
+            p2p_op.op(
+                p2p_op.tensor,
+                p2p_op.peer,
+                p2p_op.group,
+                p2p_op.tag
+            )
+    return cm.works
+
+# A lightweight hook to support batch_isend_irecv in PyTorch 2.7 and newer
+torch.distributed.batch_isend_irecv = _batch_isend_irecv
 replace_device(dist.distributed_c10d.PrefixStore, "__init__")
-try: 
-    import torch_npu
-    torch.distributed.batch_isend_irecv=_batch_isend_irecv
-    print("NPU-optimized batch_isend_irecv implementation enabled")
-except ImportError:
-    print("Using PyTorch's native batch_isend_irecv implementation")
