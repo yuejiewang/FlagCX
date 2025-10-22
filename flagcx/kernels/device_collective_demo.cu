ebc5d62e77833a37c46b62450dde81f209d6d9cf
yuejiewang <kathywangyuejie@gmail.com>
2025-10-22 16:16:53 +0800

    compile ok

--- flagcx/kernels/device_collective_demo.cu
+++ flagcx/kernels/device_collective_demo.cu
@@ -1,4 +1,5 @@
 #include "flagcx_kernel.h"
+#include "global_comm.h"
 
 __global__ void flagcxP2pDemo(const void *sendbuff, void *recvbuff,
                               size_t count, flagcxDataType_t datatype, int peer,
@@ -6,18 +7,18 @@ __global__ void flagcxP2pDemo(const void *sendbuff, void *recvbuff,
   int tid = threadIdx.x;
   if (tid == 0) {
     for (int i = 0; i < 16; i++) {
-      const void *sendaddr = const_cast<const void *>(
-          const_cast<char *>(sendbuff) +
-          count / 16 * i * getFlagcxDataTypeSize(datatype));
-      flagcxDeviceSend(sendaddr, count / 16, datatype, peer, comm);
+      const void *sendaddr = static_cast<const void *>(
+          static_cast<char *>(const_cast<void *>(sendbuff)) +
+          count / 16 * i * getFlagcxDataTypeSizeDevice(datatype));
+      flagcxDeviceSend(sendaddr, count / 16, datatype, peer, comm->hetero_comm);
     }
     for (int i = 0; i < 16; i++) {
       void *recvaddr =
-          const_cast<void *>(const_cast<char *>(recvbuff) +
-                             count / 16 * i * getFlagcxDataTypeSize(datatype));
-      flagcxDeviceRecv(recvaddr, count / 16, datatype, peer, comm);
+          static_cast<void *>(static_cast<char *>(recvbuff) +
+                             count / 16 * i * getFlagcxDataTypeSizeDevice(datatype));
+      flagcxDeviceRecv(recvaddr, count / 16, datatype, peer, comm->hetero_comm);
     }
-    flagcxDeviceTerm(comm);
-    flagcxDeviceWait(comm);
+    flagcxDeviceTerm(comm->hetero_comm);
+    flagcxDeviceWait(comm->hetero_comm);
   }
 }
--- flagcx/kernels/flagcx_kernel.cu
+++ flagcx/kernels/flagcx_kernel.cu
@@ -13,6 +13,38 @@ __device__ __forceinline__ void spin_backoff(int iter) {
 #endif
 }
 
+__device__ size_t getFlagcxDataTypeSizeDevice(flagcxDataType_t dtype) {
+  switch (dtype) {
+    // case flagcxInt8:
+    case flagcxChar:
+      return sizeof(char); // 1 byte
+    case flagcxUint8:
+      return sizeof(unsigned char); // 1 byte
+    // case flagcxInt32:
+    case flagcxInt:
+      return sizeof(int); // 4 bytes
+    case flagcxUint32:
+      return sizeof(unsigned int); // 4 bytes
+    case flagcxInt64:
+      return sizeof(long long); // 8 bytes
+    case flagcxUint64:
+      return sizeof(unsigned long long); // 8 bytes
+    // case flagcxFloat16:
+    case flagcxHalf:
+      return 2; // Half precision float is 2 bytes
+    // case flagcxFloat32:
+    case flagcxFloat:
+      return sizeof(float); // 4 bytes
+    // case flagcxFloat64:
+    case flagcxDouble:
+      return sizeof(double); // 8 bytes
+    case flagcxBfloat16:
+      return 2; // BFloat16 is typically 2 bytes
+    default:
+      return 0;
+  }
+}
+
 __device__ flagcxResult_t flagcxDeviceSend(const void *sendbuff, size_t count,
                                            flagcxDataType_t datatype, int peer,
                                            struct flagcxHeteroComm *comm) {
@@ -25,6 +57,7 @@ __device__ flagcxResult_t flagcxDeviceSend(const void *sendbuff, size_t count,
   comm->proxyKernelState->fifo->enqueue(trigger);
   return flagcxSuccess;
 }
+
 __device__ flagcxResult_t flagcxDeviceRecv(void *recvbuff, size_t count,
                                            flagcxDataType_t datatype, int peer,
                                            struct flagcxHeteroComm *comm) {
@@ -37,6 +70,7 @@ __device__ flagcxResult_t flagcxDeviceRecv(void *recvbuff, size_t count,
   comm->proxyKernelState->fifo->enqueue(trigger);
   return flagcxSuccess;
 }
+
 __device__ flagcxResult_t flagcxDeviceWait(struct flagcxHeteroComm *comm) {
   int curr_p = __ldg(comm->proxyKernelState->fifo->produced);
   int curr_c = __ldg(comm->proxyKernelState->fifo->consumed);
@@ -50,6 +84,7 @@ __device__ flagcxResult_t flagcxDeviceWait(struct flagcxHeteroComm *comm) {
   }
   return flagcxSuccess;
 }
+
 __device__ flagcxResult_t flagcxDeviceTerm(struct flagcxHeteroComm *comm) {
   flagcxDeviceTrigger trigger;
   trigger.fields.addr = 0;
@@ -69,17 +104,34 @@ __device__ flagcxResult_t flagcxFifo::enqueue(flagcxDeviceTrigger trigger) {
     if (old_p - old_c < capacity) {
       int prev = atomicCAS(produced, old_p, old_p + 1);
       if (prev == old_p) {
-        idx = old_p;
+        idx = old_p + 1;
         break;
       }
     }
   }
-  *(const_cast<flagcxDeviceTrigger_t>(buffer) + idx) = trigger;
+  *(buffer + 2 * idx) = trigger.value.fst;
+  *(buffer + 2 * idx + 1) = trigger.value.snd;
   return flagcxSuccess;
 }
 
 __host__ flagcxResult_t flagcxFifo::dequeue(flagcxDeviceTrigger_t trigger) {
-  *trigger = *(const_cast<flagcxDeviceTrigger_t>(buffer) + (++consumed[0]));
+  /*
+  int idx = -1;
+  while (true) {
+    int old_c = consumed[0];
+    int old_p = produced[0];
+    if (old_c < old_p) {
+      int prev = atomicCAS(consumed, old_c, old_c + 1);
+      if (prev == old_c) {
+        idx = old_c + 1;
+        break;
+      }
+    }
+  }
+  */
+  consumed[0]++;
+  (*trigger).value.fst = *(buffer + 2 * consumed[0]);
+  (*trigger).value.snd = *(buffer + 2 * consumed[0] + 1);
   return flagcxSuccess;
 }
 
