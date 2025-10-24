54d989aa19bfb7188c7c424181d4deaa98bebf99
yuejiewang <kathywangyuejie@gmail.com>
2025-10-24 10:30:19 +0800

    refactor host api

--- flagcx/kernels/device_collective_demo.cu
+++ flagcx/kernels/device_collective_demo.cu
@@ -1,7 +1,15 @@
+#include "flagcx.h"
 #include "flagcx_kernel.h"
 #include "global_comm.h"
+__host__ void flagcxP2pDemo(const void *sendbuff, void *recvbuff, size_t count,
+                            flagcxDataType_t datatype, int sendPeer,
+                            int recvPeer, flagcxComm_t comm,
+                            flagcxStream_t stream) {
+  flagcxP2pKernel<<<1, 1, 0, stream->base>>>(
+      sendbuff, recvbuff, count, DATATYPE, sendPeer, recvPeer, comm);
+}
 
-__global__ void flagcxP2pDemo(const void *sendbuff, void *recvbuff,
+__global__ void flagcxP2pKernel(const void *sendbuff, void *recvbuff,
                               size_t count, flagcxDataType_t datatype,
                               int sendPeer, int recvPeer, flagcxComm_t comm) {
   int tid = threadIdx.x;
