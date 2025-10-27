640f3be46cadcf3d9faa563e6ed00dcea014d7eb
yuejiewang <kathywangyuejie@gmail.com>
2025-10-27 18:55:39 +0800

    add logs for debugging

--- flagcx/core/proxy_kernel.cc
+++ flagcx/core/proxy_kernel.cc
@@ -1,34 +1,46 @@
 #include "proxy_kernel.h"
 
 void *flagcxProxyKernelService(void *args) {
-  flagcxProxyKernelServiceArgs_t ptr = (flagcxProxyKernelServiceArgs_t)args;
-  struct flagcxHeteroComm *comm = ptr->comm;
+  flagcxProxyKernelServiceArgs_t argptr = (flagcxProxyKernelServiceArgs_t)args;
+  struct flagcxHeteroComm *comm = argptr->comm;
   flagcxFifo_t fifo = comm->proxyKernelState->fifo;
-  flagcxStream_t stream = ptr->stream;
+  flagcxStream_t stream = argptr->stream;
   int groupCount = 0;
+  flagcxDeviceTrigger_t ptr;
+  flagcxCalloc(&ptr, sizeof(flagcxDeviceTrigger));
   while (true) {
     if (groupCount == 0) {
       flagcxHeteroGroupStart();
+      TRACE(FLAGCX_P2P,
+            "rank=%d flagcxHeteroGroupStart called by proxyKernelService.",
+            comm->rank);
       groupCount++;
     }
-    flagcxDeviceTrigger_t ptr;
-    flagcxCalloc(&ptr, sizeof(flagcxDeviceTrigger));
     fifo->dequeue(ptr);
     flagcxDeviceTrigger trigger = *ptr;
     switch (trigger.fields.type) {
       case flagcxDevicePrimSend:
+        TRACE(FLAGCX_P2P,
+              "rank=%d flagcxDevicePrimSend called by proxyKernelService.",
+              comm->rank);
         flagcxHeteroSend((const void *)(uintptr_t)(trigger.fields.addr),
                          trigger.fields.count,
                          (flagcxDataType_t)(trigger.fields.datatype),
                          trigger.fields.peerRank, comm, stream);
         break;
       case flagcxDevicePrimRecv:
+        TRACE(FLAGCX_P2P,
+              "rank=%d flagcxDevicePrimRecv called by proxyKernelService.",
+              comm->rank);
         flagcxHeteroRecv((void *)(uintptr_t)(trigger.fields.addr),
                          trigger.fields.count,
                          (flagcxDataType_t)(trigger.fields.datatype),
                          trigger.fields.peerRank, comm, stream);
         break;
       case flagcxDevicePrimTerm:
+        TRACE(FLAGCX_P2P,
+              "rank=%d flagcxHeteroGroupEnd called by proxyKernelService.",
+              comm->rank);
         flagcxHeteroGroupEnd();
         groupCount--;
         break;
@@ -36,6 +48,7 @@ void *flagcxProxyKernelService(void *args) {
         break;
     }
   }
+  free(ptr);
 }
 
 flagcxResult_t flagcxProxyKernelInit(struct flagcxHeteroComm *comm) {
