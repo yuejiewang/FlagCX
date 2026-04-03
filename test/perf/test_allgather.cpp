2d30dd10630a05844fb47820ab78d0be0a0d03a6
yuejiewang <kathywangyuejie@gmail.com>
2026-04-03 15:05:56 +0800

    add grouped allgather implementation

---
+++ test/perf/test_allgather.cpp
@@ -0,0 +1,177 @@
+#include "flagcx.h"
+#include "tools.h"
+#include <cstring>
+#include <iostream>
+
+#define DATATYPE flagcxFloat
+
+int main(int argc, char *argv[]) {
+  parser args(argc, argv);
+  size_t min_bytes = args.getMinBytes();
+  size_t max_bytes = args.getMaxBytes();
+  int step_factor = args.getStepFactor();
+  int num_warmup_iters = args.getWarmupIters();
+  int num_iters = args.getTestIters();
+  int print_buffer = args.isPrintBuffer();
+  uint64_t split_mask = args.getSplitMask();
+  int local_register = args.getLocalRegister();
+
+  flagcxHandlerGroup_t handler;
+  flagcxHandleInit(&handler);
+  flagcxUniqueId_t &uniqueId = handler->uniqueId;
+  flagcxComm_t &comm = handler->comm;
+  flagcxDeviceHandle_t &devHandle = handler->devHandle;
+
+  int color = 0;
+  int worldSize = 1, worldRank = 0;
+  int totalProcs = 1, proc = 0;
+  MPI_Comm splitComm;
+  initMpiEnv(argc, argv, worldRank, worldSize, proc, totalProcs, color,
+             splitComm, split_mask);
+
+  int nGpu;
+  devHandle->getDeviceCount(&nGpu);
+  devHandle->setDevice(worldRank % nGpu);
+
+  if (proc == 0)
+    flagcxGetUniqueId(&uniqueId);
+  MPI_Bcast((void *)uniqueId, sizeof(flagcxUniqueId), MPI_BYTE, 0, splitComm);
+  MPI_Barrier(MPI_COMM_WORLD);
+
+  flagcxCommInitRank(&comm, totalProcs, uniqueId, proc);
+
+  flagcxStream_t stream;
+  devHandle->streamCreate(&stream);
+
+  void *sendbuff = nullptr;
+  void *recvbuff = nullptr;
+  void *hello = nullptr;
+  void *sendHandle = nullptr;
+  void *recvHandle = nullptr;
+  size_t count;
+  timer tim;
+
+  if (local_register) {
+    // allocate buffer
+    flagcxMemAlloc(&sendbuff, max_bytes / totalProcs);
+    flagcxMemAlloc(&recvbuff, max_bytes);
+    // register buffer
+    flagcxCommRegister(comm, sendbuff, max_bytes / totalProcs, &sendHandle);
+    flagcxCommRegister(comm, recvbuff, max_bytes, &recvHandle);
+  } else {
+    devHandle->deviceMalloc(&sendbuff, max_bytes / totalProcs, flagcxMemDevice,
+                            NULL);
+    devHandle->deviceMalloc(&recvbuff, max_bytes, flagcxMemDevice, NULL);
+  }
+  hello = malloc(max_bytes);
+  memset(hello, 0, max_bytes);
+
+  // Warm-up for large size
+  for (int i = 0; i < num_warmup_iters; i++) {
+    flagcxAllGather(sendbuff, recvbuff,
+                    (max_bytes / sizeof(float)) / totalProcs, DATATYPE, comm,
+                    stream);
+  }
+  devHandle->streamSynchronize(stream);
+
+  // Warm-up for small size
+  for (int i = 0; i < num_warmup_iters; i++) {
+    flagcxAllGather(sendbuff, recvbuff,
+                    (min_bytes / sizeof(float)) / totalProcs, DATATYPE, comm,
+                    stream);
+  }
+  devHandle->streamSynchronize(stream);
+
+  for (size_t size = min_bytes; size <= max_bytes; size *= step_factor) {
+    count = size / sizeof(float);
+    size_t count_per_rank = count / totalProcs;
+
+    memset(hello, 0, size / totalProcs);
+    ((float *)hello)[0] = proc;
+
+    devHandle->deviceMemcpy(sendbuff, hello, size / totalProcs,
+                            flagcxMemcpyHostToDevice, NULL);
+
+    if ((proc == 0 || proc == totalProcs - 1) && color == 0 && print_buffer) {
+      printf("sendbuff = ");
+      printf("%f\n", ((float *)hello)[0]);
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    tim.reset();
+    for (int i = 0; i < num_iters; i++) {
+      flagcxAllGather(sendbuff, recvbuff, count / totalProcs, DATATYPE, comm,
+                      stream);
+    }
+    devHandle->streamSynchronize(stream);
+
+    double elapsed_time = tim.elapsed() / num_iters;
+    MPI_Allreduce(MPI_IN_PLACE, (void *)&elapsed_time, 1, MPI_DOUBLE, MPI_SUM,
+                  MPI_COMM_WORLD);
+    elapsed_time /= worldSize;
+
+    double base_bw = (double)(size) / 1.0E9 / elapsed_time;
+    double alg_bw = base_bw;
+    double factor = ((double)(totalProcs - 1)) / ((double)totalProcs);
+    double bus_bw = base_bw * factor;
+    if (proc == 0 && color == 0) {
+      printf("Comm size: %zu bytes; Elapsed time: %lf sec; Algo bandwidth: %lf "
+             "GB/s; Bus bandwidth: %lf GB/s\n",
+             size, elapsed_time, alg_bw, bus_bw);
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    memset(hello, 0, size);
+    devHandle->deviceMemcpy(hello, recvbuff, size, flagcxMemcpyDeviceToHost,
+                            NULL);
+
+    int correct = 1;
+    for (size_t src_rank = 0; src_rank < (size_t)totalProcs; src_rank++) {
+      for (size_t offset = 0; offset < count_per_rank; offset++) {
+        size_t recv_offset = src_rank * count_per_rank + offset;
+        float expected = offset == 0 ? (float)src_rank : 0.0f;
+        float actual = ((float *)hello)[recv_offset];
+        if (actual != expected) {
+          printf("rank %d wrong output at offset %zu (src rank %zu, local "
+                 "offset %zu), expected %f, got %f\n",
+                 proc, recv_offset, src_rank, offset, expected, actual);
+          correct = 0;
+          break;
+        }
+      }
+      if (!correct) {
+        break;
+      }
+    }
+
+    if ((proc == 0 || proc == totalProcs - 1) && color == 0 && print_buffer) {
+      printf("recvbuff = ");
+      for (int i = 0; i < totalProcs; i++) {
+        printf("%f ", ((float *)hello)[i * count_per_rank]);
+      }
+      printf("\n");
+      printf("rank %d all-gather correctness = %d\n", proc, correct);
+    }
+  }
+
+  if (local_register) {
+    // deregister buffer
+    flagcxCommDeregister(comm, sendHandle);
+    flagcxCommDeregister(comm, recvHandle);
+    // deallocate buffer
+    flagcxMemFree(sendbuff);
+    flagcxMemFree(recvbuff);
+  } else {
+    devHandle->deviceFree(sendbuff, flagcxMemDevice, NULL);
+    devHandle->deviceFree(recvbuff, flagcxMemDevice, NULL);
+  }
+  free(hello);
+  devHandle->streamDestroy(stream);
+  flagcxCommDestroy(comm);
+  flagcxHandleFree(handler);
+
+  MPI_Finalize();
+  return 0;
+}
